99eada64b625564dfd9ebccb36ddeed74a21ca3c
Mike Nye <mike.nye@gmail.com>
2020-06-09 09:19:04 +0800

    Update testing.

--- test.sh
+++ test.sh
@@ -6,13 +6,19 @@ docker context use x86_64
 export DOCKER_CLI_EXPERIMENTAL="enabled"
 docker buildx use homecluster
 
+echo "========== Testing linux/386 distros =========="
+docker buildx build --no-cache --progress plain -f Dockerfile.testing --platform linux/386 . || exit 1
+
 echo "========== Testing linux/amd64 distros =========="
-docker buildx build --no-cache --progress plain -f Dockerfile.testing.amd64 --platform linux/amd64 . || exit 1
+docker buildx build --no-cache --progress plain -f Dockerfile.testing --platform linux/amd64 . || exit 1
 
 echo "========== Testing linux/arm64 distros =========="
-docker buildx build --no-cache --progress plain -f Dockerfile.testing.arm64.v8 --platform linux/arm64 . || exit 1
+docker buildx build --no-cache --progress plain -f Dockerfile.testing --platform linux/arm64 . || exit 1
 
 echo "========== Testing linux/amd/v7 distros =========="
-docker buildx build --no-cache --progress plain -f Dockerfile.testing.arm.v7 --platform linux/arm/v7 . || exit 1
+docker buildx build --no-cache --progress plain -f Dockerfile.testing --platform linux/arm/v7 . || exit 1
+
+echo "========== Testing linux/amd/v6 distros =========="
+docker buildx build --no-cache --progress plain -f Dockerfile.testing --platform linux/arm/v7 . || exit 1
 
 echo "========== COMPLETED! =========="
